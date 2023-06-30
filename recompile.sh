e5b46e0432f976778fb3db9a3446bb9347844b33
knpfalcon <knpfalcon@gmail.com>
2023-06-30 13:07:54 -0400

    Initial Commit. Moving this to its own repo.

---
+++ recompile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export PATH=/home/knpfalcon/djgpp/i586-pc-msdosdjgpp/bin/:$PATH
+export GCC_EXEC_PREFIX=/home/knpfalcon/djgpp/lib/gcc/
+
+make veryclean TARGET_ARCH_EXCL=i386 UNIX_TOOLS=1 CROSSCOMPILE=1
+make depend TARGET_ARCH_EXCL=i386 UNIX_TOOLS=1 CROSSCOMPILE=1
+make lib TARGET_ARCH_EXCL=i386 UNIX_TOOLS=1 CROSSCOMPILE=1
+
+cp /home/knpfalcon/djgpp/allegro/lib/djgpp/liballeg.a /home/knpfalcon/djgpp/lib/
