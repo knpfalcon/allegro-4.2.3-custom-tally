c63c52e9ef7bf8a45c584f43ef134b17699a1dcd
knpfalcon <knpfalcon@gmail.com>
2023-06-30 17:42:50 -0400

    recompile now copies also includes to my needed directory.

--- recompile.sh
+++ recompile.sh
@@ -8,3 +8,4 @@ make depend TARGET_ARCH_EXCL=i386 UNIX_TOOLS=1 CROSSCOMPILE=1
 make lib TARGET_ARCH_EXCL=i386 UNIX_TOOLS=1 CROSSCOMPILE=1
 
 cp /home/knpfalcon/djgpp/allegro/lib/djgpp/liballeg.a /home/knpfalcon/djgpp/lib/
+cp -r /home/knpfalcon/djgpp/allegro/include/ /home/knpfalcon/djgpp/
